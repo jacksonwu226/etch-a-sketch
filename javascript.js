c446a04ea699701e754afc219be1414b1d3f8e2d
Jackson Wu <jacksonwu226@gmail.com>
2023-10-18 16:58:43 -0700

    used new method to remove existing grid - just to see if the forEach() method would work here
    added input verification to see if input is >100 <0 and if input is a valid number

--- javascript.js
+++ javascript.js
@@ -1,7 +1,18 @@
 const container = document.querySelector('.container')
 const new_size_btn = document.querySelector('.set-grid-size-btn')
-const new_size = document.querySelector('.set-grid-size')
+const new_size = document.querySelector('#set-grid-size-input')
 
+function deleteGrid(container){
+  // let node = container.firstElementChild
+  // while(node){
+  //   node.remove();
+  //   node = container.firstElementChild
+  // }
+  const everyRow = container.querySelectorAll('.rows')
+  everyRow.forEach(row => {
+    row.remove()
+  })
+}
 
 function createGrid(size)
 {
@@ -19,5 +30,16 @@ function createGrid(size)
     container.appendChild(row)
   }
 }
+
+new_size_btn.addEventListener('click', () =>{
+  let n = parseInt(new_size.value)
+  if(n > 100 || isNaN(n) || n <1 )
+    n = 16
+  new_size.value = ''
+  deleteGrid(container)
+  createGrid(n)
+  new_size.focus()
+})
+
 
 createGrid(16)
