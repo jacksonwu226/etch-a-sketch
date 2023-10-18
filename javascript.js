09de5883764c72fb1ae55f575c8186b92e1d3e0e
Jackson Wu <jacksonwu226@gmail.com>
2023-10-18 16:35:19 -0700

    finished css styling so that when mouse hovers over the boxes, it will change from white to black

--- javascript.js
+++ javascript.js
@@ -1,9 +1,23 @@
+const container = document.querySelector('.container')
+const new_size_btn = document.querySelector('.set-grid-size-btn')
+const new_size = document.querySelector('.set-grid-size')
+
+
 function createGrid(size)
 {
   for(let i = 0; i < size; ++i){
+    const row = document.createElement('div')
+    row.setAttribute('class', 'rows')
     for(let j = 0; j < size; ++j){
-      const div = document.createElement('div')
-      div.setAttribute('class','boxes')
+      const col = document.createElement('div')
+      col.setAttribute('class','cols')
+      row.appendChild(col)
+      col.addEventListener('mouseover', ()=>{
+        col.style.backgroundColor = 'black'
+      })
     }
+    container.appendChild(row)
   }
 }
+
+createGrid(16)
